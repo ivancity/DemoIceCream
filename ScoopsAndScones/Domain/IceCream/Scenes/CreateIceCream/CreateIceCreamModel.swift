8da44b1c19769183a0d0cb0bbc045e09a0d5cc43
ivancity <shieldm@hotmail.com>
2022-07-13 18:18:27 -0500

    first commit

---
+++ ScoopsAndScones/Domain/IceCream/Scenes/CreateIceCream/CreateIceCreamModel.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+enum CreateIceCream {
+  enum LoadIceCream {
+    struct Request {}
+    
+    struct Response {
+      var iceCreamData: IceCream
+    }
+    
+    struct ViewModel {
+      var cones: [String]
+      var flavors: [String]
+      var toppings: [String]
+    }
+  }
+}
