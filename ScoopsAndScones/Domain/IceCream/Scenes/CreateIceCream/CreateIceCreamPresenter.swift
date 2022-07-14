a8abe7c49f151f39767282ca404ce237f7346d9e
ivancity <shieldm@hotmail.com>
2022-07-13 22:54:45 -0500

    adding the CreateIceCreamPresenter implementation that conforms to CreateIceCreamPresentationLogic protocol

--- ScoopsAndScones/Domain/IceCream/Scenes/CreateIceCream/CreateIceCreamPresenter.swift
+++ ScoopsAndScones/Domain/IceCream/Scenes/CreateIceCream/CreateIceCreamPresenter.swift
@@ -7,3 +7,14 @@ protocol CreateIceCreamPresentationLogic {
 class CreateIceCreamPresenter {
   var view: CreateIceCreamDisplayLogic?
 }
+
+extension CreateIceCreamPresenter: CreateIceCreamPresentationLogic {
+  func presentIceCream(response: CreateIceCream.LoadIceCream.Response) {
+    let viewModel = CreateIceCream.LoadIceCream.ViewModel(
+      cones: response.iceCreamData.cones,
+      flavors: response.iceCreamData.flavors,
+      toppings: response.iceCreamData.toppings
+    )
+    view?.displayIceCream(viewModel: viewModel)
+  }
+}
